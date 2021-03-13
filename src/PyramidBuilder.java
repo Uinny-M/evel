74e1ff396900da404b12cc5c211b10429fe87a5e
Uinny-M <margaret_ne@rambler.ru>
2021-03-13 23:19:19 +0300

    Initial commit

---
+++ src/PyramidBuilder.java
@@ -0,0 +1,41 @@
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class PyramidBuilder {
+
+    /**
+     * Builds a pyramid with sorted values (with minumum value at the top line and maximum at the bottom,
+     * from left to right). All vacant positions in the array are zeros.
+     *
+     * @param inputNumbers to be used in the pyramid
+     * @return 2d array with pyramid inside
+     *
+     */
+    public int[][] buildPyramid(List<Integer> inputNumbers) {
+        // TODO : Implement your solution here
+
+        int height = 0, width = 0;
+        double doubleHeight = (Math.sqrt(1 + 8 * inputNumbers.size()) - 1) / 2;
+
+        if (doubleHeight % 1 != 0 || inputNumbers.contains(null)) System.out.println("ups");//throw new CannotBuildPyramidException();
+        else {
+            height = (int) doubleHeight;
+            width = height * 2 - 1;
+        }
+
+        int[][] result = new int[height][width];
+        Collections.sort(inputNumbers);
+        Queue<Integer> queue = new LinkedList<>(inputNumbers);
+
+        for (int i = 0; i < height; i++) {
+            int j = height - i - 1;
+            while ((j >= height - i - 1) && (j <= width - (height - i))) {
+                result[i][j] = queue.poll();
+                j+=2;
+            }
+        }
+        return result;
+    }
+}
---
+++ src/Subsequence.java
@@ -0,0 +1,30 @@
+import java.util.List;
+
+public class Subsequence {
+    /**
+     * Checks if it is possible to get a sequence which is equal to the first
+     * one by removing some elements from the second one.
+     *
+     * @param x first sequence
+     * @param y second sequence
+     * @return <code>true</code> if possible, otherwise <code>false</code>
+     */
+    @SuppressWarnings("rawtypes")
+    public boolean find(List x, List y) {
+        // TODO: Implement the logic here
+        if (x==null||y==null) throw new IllegalArgumentException();
+        if (!y.containsAll(x)||x.size() > y.size()) return false;
+        if (x.size() == 0) return true;
+
+        for (int i = 0, j = 0; i < y.size(); i++) {
+            if (y.get(0) instanceof String) {
+                if (y.get(i).equals(x.get(j)) && j < x.size() - 1) j++;
+                if (y.get(i).equals(x.get(j)) && j == x.size() - 1) return true;
+            } else {
+                if (y.get(i) == x.get(j) && j < x.size() - 1) j++;
+                if (y.get(i) == x.get(j) && j == x.size() - 1) return true;
+            }
+        }
+        return false;
+    }
+}
